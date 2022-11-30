befe9d300c35fbe8a08f14035f3b973dac987f82
Zij-IT <elijah.reed@hartvigsen.xyz>
2022-11-30 23:27:59 +0100

    ModifySub: move date parsing from add into commands/mod.rs

--- src/commands/add.rs
+++ src/commands/add.rs
@@ -1,8 +1,5 @@
-use super::{Database, Local, NaiveDate, Priority, Result, Status, ToDo};
-use chrono::{Datelike, NaiveDateTime};
+use super::{date_from_input, Database, Local, Priority, Result, Status, ToDo};
 use clap::Args;
-
-const SHORT_DAYS: [&str; 7] = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
 
 #[derive(Debug, Args)]
 #[command(
@@ -50,7 +47,7 @@ pub struct Add {
 pub fn add(command: &Add) -> Result<()> {
     let desc = command.todo.clone();
 
-    let start: NaiveDateTime = command
+    let start = command
         .start_date
         .as_deref()
         .map_or_else(|| Local::now().naive_local(), date_from_input);
@@ -72,23 +69,4 @@ pub fn add(command: &Add) -> Result<()> {
     };
 
     Database::from_clido_dir()?.add(todo).save()
-}
-
-fn date_from_input(date: &str) -> NaiveDateTime {
-    if SHORT_DAYS.contains(&&date[..3]) {
-        let now = Local::now().naive_local();
-        day_to_date(&now, date).unwrap_or(now)
-    } else {
-        let naive = NaiveDate::parse_from_str(date, "%d-%m-%Y").unwrap();
-        naive.and_time(Local::now().naive_local().time())
-    }
-}
-
-fn day_to_date(curr_date: &NaiveDateTime, desired_date: &str) -> Option<NaiveDateTime> {
-    let goal = &desired_date.to_ascii_lowercase()[..3];
-    curr_date
-        .date()
-        .iter_days()
-        .find(|x| SHORT_DAYS[x.weekday() as usize] == goal)
-        .map(|x| x.and_time(Local::now().naive_local().time()))
 }
--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -6,10 +6,31 @@ mod mark;
 use super::db::{Database, Priority, Status, ToDo};
 
 use anyhow::Result;
-use chrono::{Local, NaiveDate};
+use chrono::{Datelike, Local, NaiveDate, NaiveDateTime};
 use prettytable::{format, Table};
 
 pub use add::{add, Add};
 pub use del::{del, Delete};
 pub use list::{list, List};
 pub use mark::{mark, Mark};
+
+const SHORT_DAYS: [&str; 7] = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+fn date_from_input(date: &str) -> NaiveDateTime {
+    if SHORT_DAYS.contains(&&date[..3]) {
+        let now = Local::now().naive_local();
+        day_to_date(&now, date).unwrap_or(now)
+    } else {
+        let naive = NaiveDate::parse_from_str(date, "%d-%m-%Y").unwrap();
+        naive.and_time(Local::now().naive_local().time())
+    }
+}
+
+fn day_to_date(curr_date: &NaiveDateTime, desired_date: &str) -> Option<NaiveDateTime> {
+    let goal = &desired_date.to_ascii_lowercase()[..3];
+    curr_date
+        .date()
+        .iter_days()
+        .find(|x| SHORT_DAYS[x.weekday() as usize] == goal)
+        .map(|x| x.and_time(Local::now().naive_local().time()))
+}
